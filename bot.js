f75af22c00e8c1547509d41cc354b2d0f88e69c7
MrD7z <39459185+MrD7z@users.noreply.github.com>
2018-08-14 13:58:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ m.sendMessage(args)
 
         
         client.on('message',async message => {
-    var codes = "^";
+    var codes = "#^";
     const sev = message.guild.channels.get(process.env.SEV);
     const ev = message.guild.channels.get('478434961142054912');
     const evc = message.guild.channels.get('476725999610888213');
